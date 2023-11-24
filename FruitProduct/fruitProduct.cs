017c65ac878af73a40c432474c5772f96293c4a8
faisl <faisal@gmail.com>
2023-11-24 00:49:58 -0800

    Add project files.

---
+++ FruitProduct/fruitProduct.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment.FruitProduct
+{
+    internal class fruitProduct
+    { 
+         public class Fruit
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public string Taste { get; set; }
+        public bool IsEdible { get; set; }
+
+        public Fruit(string name, string color, string taste, bool isEdible)
+        {
+            Name = name;
+            Color = color;
+            Taste = taste;
+            IsEdible = isEdible;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Color: {Color}");
+            Console.WriteLine($"Taste: {Taste}");
+            Console.WriteLine($"Edible: {(IsEdible ? "Yes" : "No")}");
+        }
+
+        public void ChangeColor(string newColor)
+        {
+            Color = newColor;
+        }
+
+        public void SetEdible(bool edibleStatus)
+        {
+            IsEdible = edibleStatus;
+        }
+    }
+
+}
+
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Assigment.FruitProduct.fruitProduct;
+
+using System;
+
+namespace Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Fruit apple = new Fruit("Apple", "Red", "Sweet", true);
+            Fruit orange = new Fruit("Orange", "Orange", "Tangy", true);
+            Fruit banana = new Fruit("Banana", "Yellow", "Sweet", true);
+            Fruit Strawberry = new Fruit("Strawberry", "Red", "Sweet", true);
+
+
+
+            apple.DisplayInfo();
+            orange.DisplayInfo();
+            banana.DisplayInfo();
+
+
+            apple.ChangeColor("Green");
+            apple.SetEdible(false);
+
+
+        }
+    }
+}
+
+
+
